4ff2d44a86aa9e2a4e284e00932b8586a7651365
MrKalleP <kallle.v.pettersson@gmail.com>
2024-12-17 13:43:45 +0100

    fixed logo and some colors for frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from 'antd'
 
 function App() {
 
   return (
     <>
       <h2>Hello world!</h2>
+      <div>
+        <Button type="primary" size='large'>Klicka här</Button>
+      </div>
     </>
   )
 }
