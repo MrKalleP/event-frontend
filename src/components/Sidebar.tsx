35fdac363471143a1f9a53560aefc543249942c1
MrKalleP <kallle.v.pettersson@gmail.com>
2024-12-17 15:22:11 +0100

    fixed home and projects btn

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react';
 
 import {
     DesktopOutlined,
-    FileOutlined,
     PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
@@ -29,15 +26,8 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-        getItem('Tom', '3'),
-        getItem('Bill', '4'),
-        getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    getItem('Home', '1', <PieChartOutlined />),
+    getItem('Projects', '2', <DesktopOutlined />)
 ];
 
 function SideBar() {
