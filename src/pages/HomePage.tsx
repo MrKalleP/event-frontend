0aad5c2bba034c34b5302274eded780dfc04dfbb
MrKalleP <kallle.v.pettersson@gmail.com>
2024-12-19 10:57:22 +0100

    fixed some standard chart for home page not the correct data yet

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,9 +5,10 @@ import { Row, Col } from "antd";
 import TableHomePage from "../components/TableHomePage";
 import { CrashedLoggs } from "../components/CrashedLoggs"
 import { InfoLoggs } from "../components/InfoLoggs"
+import { DemoLine } from "../components/LineChart";
 
 const HomePage = () => {
-    return <Row gutter={[24, 24]} justify="center" align="middle">
+    return <Row gutter={[24, 24]} justify="center" align="middle" style={{ backgroundColor: "#07495b", borderRadius: ".5rem", padding: "1rem", color: "white" }}>
         <Col span={6}>
             <h2>This is all loggs</h2>
             <InfoLoggs />
@@ -24,8 +25,11 @@ const HomePage = () => {
             <h2>This is all the crached loggs</h2>
             <CrashedLoggs />
         </Col>
-        <Col span={24}>
+        <Col span={12}>
             <TableHomePage />
+        </Col>
+        <Col span={12} style={{ background: "#fff", borderRadius: ".5rem", padding: "2.3rem", marginBottom: "4rem" }}>
+            <DemoLine />
         </Col>
 
     </Row>
