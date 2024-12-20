2235ea6b8f3dedf66e727b726e302c948a639e21
MrKalleP <kallle.v.pettersson@gmail.com>
2024-12-20 08:58:31 +0100

    try to fix my data try to fix lines for type

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,6 +1,5 @@
-import { PureComponent } from 'react';
-import test_data from "../utils/testdata.json"
-
+import { PureComponent } from "react";
+import test_data from "../utils/testdata.json";
 import {
     LineChart,
     Line,
@@ -10,18 +9,42 @@ import {
     Tooltip,
     Legend,
     ResponsiveContainer,
-} from 'recharts';
+} from "recharts";
 
 
+const processData = (test_data: any[]) => {
+    const today = new Date();
+    const sevenDaysAhead = new Date();
+    sevenDaysAhead.setDate(today.getDate() + 7);
+
+    const aggregatedData: any = {};
+
+    test_data.forEach((item) => {
+        const itemDate = new Date(item.date);
+        if (itemDate >= today && itemDate <= sevenDaysAhead) {
+            const dateKey = itemDate.toISOString().split("T")[0];
+
+            if (!aggregatedData[dateKey]) {
+                aggregatedData[dateKey] = { date: dateKey, error: 0, warning: 0, info: 0, crashed: 0 };
+            }
+
+            if (item.type in aggregatedData[dateKey]) {
+                aggregatedData[dateKey][item.type] += 1;
+            }
+        }
+    });
+
+    return Object.values(aggregatedData);
+};
 
 class LineChartExample extends PureComponent {
     render() {
+        const chartData = processData(test_data);
+
         return (
-            <ResponsiveContainer width="100%" height="100%">
+            <ResponsiveContainer width="100%" height={400}>
                 <LineChart
-                    width={600}
-                    height={300}
-                    data={test_data}
+                    data={chartData}
                     margin={{
                         top: 5,
                         right: 30,
@@ -31,11 +54,13 @@ class LineChartExample extends PureComponent {
                 >
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="date" />
-                    <YAxis />
+                    <YAxis dataKey="type" />
                     <Tooltip />
                     <Legend />
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="error" stroke="#ff0000" name="Errors" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="warning" stroke="#ffa500" name="Warnings" />
+                    <Line type="monotone" dataKey="info" stroke="#0000ff" name="Info" />
+                    <Line type="monotone" dataKey="crashed" stroke="#800080" name="Crashed" />
                 </LineChart>
             </ResponsiveContainer>
         );
