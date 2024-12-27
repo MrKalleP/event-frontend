ac45a3f2bc1c550705e1d6e2a07b279a5063d131
MrKalleP <kallle.v.pettersson@gmail.com>
2024-12-27 13:34:49 +0100

    fixed some responsiv grid with ant designs own grid system and added overflow auto

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,22 +10,22 @@ import LineChartExample from "../components/LineChart"
 
 const HomePage = () => {
     return <Row gutter={[8, 17]} justify="center" align="middle" style={{ backgroundColor: "#002140", borderRadius: ".5rem", color: "white", margin: ".5rem", padding: ".3rem" }}>
-        <Col span={6}>
+        <Col xs={24} sm={12} md={12} lg={6}>
             <InfoLoggs />
         </Col>
-        <Col span={6}>
+        <Col xs={24} sm={12} md={12} lg={6}>
             <WarningHome />
         </Col>
-        <Col span={6}>
+        <Col xs={24} sm={12} md={12} lg={6}>
             <ErrorsHome />
         </Col>
-        <Col span={6}>
+        <Col xs={24} sm={12} md={12} lg={6}>
             <CrashedLoggs />
         </Col>
-        <Col span={12} style={{ borderRadius: ".5rem" }}>
+        <Col xs={24} sm={24} md={24} lg={12} style={{ borderRadius: ".5rem", overflow: "auto", backgroundColor: "white" }}>
             <TableHomePage />
         </Col>
-        <Col span={12} style={{ background: "#002140", borderRadius: ".5rem", height: "70vh", padding: ".7rem" }}>
+        <Col xs={24} sm={24} md={24} lg={12} style={{ background: "#002140", overflow: "auto", borderRadius: ".5rem", height: "70vh", padding: ".7rem" }}>
             < LineChartExample />
         </Col>
 
