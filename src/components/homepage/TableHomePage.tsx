989d81dac5d721d6c00a8d6d061f6256f17edb9a
MrKalleP <kallle.v.pettersson@gmail.com>
2025-01-17 13:21:21 +0100

    fixed some styles

--- src/components/homepage/TableHomePage.tsx
+++ src/components/homepage/TableHomePage.tsx
@@ -1,10 +1,10 @@
+import React from 'react';
 import { Table } from 'antd';
 import type { TableProps } from 'antd';
 import { DataType } from "../../utils/Interface";
-import ProjectColumns from "../projects/ProjectColumns"
+import ProjectColumns from "../projects/ProjectColumns";
 import LogDetailsModal from "../../utils/LogDetailsModal";
 import useModal from "../../utils/ModalFunctionality";
-
 
 const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
     console.log('params', pagination, filters, sorter, extra);
@@ -14,7 +14,18 @@ const TableHomePage = ({ data }) => {
     const { selectedLog, isModalOpen, showModal, handleModalClose } = useModal();
 
     return (
-        <>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                minHeight: "60vh",
+                background: "white",
+                color: "#2A2A2A",
+                borderRadius: ".5rem",
+                padding: "1rem",
+            }}
+        >
             <Table<DataType>
                 columns={ProjectColumns}
                 dataSource={data}
@@ -24,13 +35,19 @@ const TableHomePage = ({ data }) => {
                     },
                 })}
                 onChange={onChange}
+                pagination={{
+                    position: ["bottomCenter"],
+                }}
                 showSorterTooltip={{ title: 'Click to sort' }}
                 rowKey={(record) => String(record.id)}
-                style={{ background: "white", color: "#2A2A2A", borderRadius: ".5rem" }} />
+                style={{
+                    background: "white",
+                    color: "#2A2A2A",
+                }}
+            />
             <LogDetailsModal log={selectedLog} isOpen={isModalOpen} onClose={handleModalClose} />
-        </>
+        </div>
     );
-
 };
 
 export default TableHomePage;
