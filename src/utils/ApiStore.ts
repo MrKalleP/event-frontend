7f9f9666eca71c08021e38326578534101e67bbb
MrKalleP <kallle.v.pettersson@gmail.com>
2025-03-26 08:45:06 +0100

    fixed some stuff

--- src/utils/ApiStore.ts
+++ src/utils/ApiStore.ts
@@ -61,7 +61,7 @@ export const FetchProjectById = async (projectId: string) => fetchData(`projects
 
 export const fetchOneUserForProject = async (userId: string) => fetchData(`users/${userId}`);
 
-export const FetchAllLogsForThisUser = async (userId: string, projectId: string) => fetchData(`logs/${userId}/${projectId}`);
+export const FetchAllLogsForThisUser = async (userId: string, projectId: string) => fetchData(`logs/${projectId}/${userId}`);
 
 export const FetchAllLogsByTypeForOneUser = async (projectId: string, type: string) => fetchData(`logs/${projectId}/${type}`);
 
